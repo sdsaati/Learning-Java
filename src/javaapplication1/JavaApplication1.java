11f1715f210fd1fe097c6f7fba9d400ee36ef3f2
Saeid <sdsaati@gmail.com>
2015-07-25 18:56:56 +0430

    First

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Saeid
+ */
+public class JavaApplication1 extends php implements cls {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JavaApplication1 a = new JavaApplication1();
+        a.saeid();
+     
+    }
+    public void saeid(){
+        this.run();
+    }
+    public void run(){
+        System.out.print("Hello World");
+    }
+    
+}
+
